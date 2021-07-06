3c9fc513027cbdc3900f054fb990d3f853fea675
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-07-06 17:49:53 -0400

    HTML and CSS written for in-class example featuring Al Pacino

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (() => {
 
+  let littlefriend = document.querySelector('audio'),
+      playbutton = document.querySelector('.playanimation');
 
+
+
+  function playAudio() {
+    document.querySelector("#tonyvector").classList.add('gotonygo');
+    littlefriend.play();
+  }
+
+  playbutton.addEventListener("click", playAudio);
 })();
