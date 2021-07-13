05b271bdc77a18fdac87ed2f94bd9a98d83a2417
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-07-13 17:36:42 -0400

    inclass tony montana test all finished

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 
   function playAudio() {
     document.querySelector("#tonyvector").classList.add('gotonygo');
+    document.querySelectorAll('path').forEach(path => path.classList.add('textanimation'));
+    document.querySelector("#typelayout").classList.add('containerturn');
     littlefriend.play();
   }
 
