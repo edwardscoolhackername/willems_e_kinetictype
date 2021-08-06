db7115237f686a08cf911b8cba66e2d7fd4671da
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-08-06 14:19:59 -0400

    blooddrop animation finished and text animation started

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 (() => {
 
-  let littlefriend = document.querySelector('audio'),
+  let music = document.querySelector('audio'),
       playbutton = document.querySelector('.playanimation');
 
 
 
-  function playAudio() {
+  function playaudiotony() {
     document.querySelector("#tonyvector").classList.add('gotonygo');
     document.querySelectorAll('path').forEach(path => path.classList.add('textanimation'));
     document.querySelector("#typelayout").classList.add('containerturn');
     littlefriend.play();
   }
+  function playaudiowill() {
+    document.querySelector("#blooddrop").classList.add('drip');
+    document.querySelectorAll('g').forEach(group => group.classList.add('textanimation'));
+    music.play();
+  }
 
-  playbutton.addEventListener("click", playAudio);
+  playbutton.addEventListener("click", playaudiowill);
 })();
